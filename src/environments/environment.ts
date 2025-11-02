31ff25917f96310522712446f57347f48ff57a36
Jorgo69 <ibralejorgo@gmail.com>
2025-11-02 23:40:10 +0100

    second deploy ..|

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8000/api' // ← ton backend Laravel
-  apiUrl: 'https://space-x-back-end-laravel.onrender.com'
+  apiUrl: 'https://space-x-back-end-laravel.onrender.com/api'
 };
