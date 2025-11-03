c29708c85bfa4cf4ec575ee820d0151e40553a23
Jorgo69 <ibralejorgo@gmail.com>
2025-11-03 09:42:01 +0100

    refont dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,27 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { Auth } from './services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('spacex-dashboard');
   private auth = inject(Auth);
+  constructor(private router: Router) {}
 
   // Récupère l'utilisateur connecté (pour affichage)
   user() {
     return this.auth.getUser();
+  }
+
+  // Fonction pour naviguer vers la page de login
+  navigateToLogin() {
+    this.router.navigate(['/login']);
   }
 
   // Méthode appelée au clic sur "Déconnexion"
